a0664beba8ff23683d3858c703a8480bd9e23a03
MARK777 <mark1000002@gmail.com>
2023-03-19 13:05:41 +0800

    Готово

--- src/main/java/pro/sky/calculatorspring/CalculatorController.java
+++ src/main/java/pro/sky/calculatorspring/CalculatorController.java
@@ -1,10 +1,12 @@
 package pro.sky.calculatorspring;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/calculator")
 public class CalculatorController {
     private final CalculatorService calculatorService;
 
@@ -12,26 +14,26 @@ public class CalculatorController {
         this.calculatorService = calculatorService;
     }
 
-    @GetMapping(path = "/calculator")
+    @GetMapping
     public String helloCalculator() {
         return calculatorService.helloCalculator();
     }
 
-    @GetMapping(path = "/calculator/plus")
-    public String plusCalculator(@RequestParam ("num1") double number1, @RequestParam("num2") double number2) {
+    @GetMapping(path = "plus")
+    public String plusCalculator (@RequestParam ("num1") double number1, @RequestParam("num2") double number2) {
         return number1 + " + " + number2 + " = " + calculatorService.plusCalculator(number1, number2);
     }
-    @GetMapping(path = "/calculator/minus")
+    @GetMapping(path = "minus")
     public String minusCalculator(@RequestParam("num1") double number1, @RequestParam("num2") double number2) {
         return number1 + " - " + number2 + " = " + calculatorService.minusCalculator(number1, number2);
     }
 
-    @GetMapping(path = "/calculator/multiply")
+    @GetMapping(path = "multiply")
     public String multiplyCalculator(@RequestParam("num1") double number1, @RequestParam("num2") double number2) {
         return number1 + " * " + number2 + " = " + calculatorService.multiplyCalculator(number1, number2);
     }
 
-    @GetMapping(path = "/calculator/divide")
+    @GetMapping(path = "divide")
     public String divideCalculator(@RequestParam("num1") double number1, @RequestParam("num2") double number2) {
         return number1 + " / " + number2 + " = " + calculatorService.divideCalculator(number1, number2);
     }
